4c1069906383fa6f8c30e13bb53581b84759af00
angular-cli <angular-cli@angular.io>
2016-09-21 11:11:43 +0200

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { Ng2PackPage } from './app.po';
+
+describe('ng2-pack App', function() {
+  let page: Ng2PackPage;
+
+  beforeEach(() => {
+    page = new Ng2PackPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
