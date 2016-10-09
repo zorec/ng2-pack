a2ef2c9a8fe8df356ad92ab4b5683ca0ee839529
Gabriel Brandersky <brandersky@interfacewerk>
2016-10-09 21:41:15 +0200

    fix(table): fix failing tests

--- src/lib/select2/select2.component.spec.ts
+++ src/lib/select2/select2.component.spec.ts
@@ -2,9 +2,11 @@
 
 import { Select2Component } from './select2.component';
 
+import {ElementRef} from '@angular/core';
+
 describe('Component: Ng2Select2', () => {
   it('should create an instance', () => {
-    let component = new Select2Component();
+    let component = new Select2Component(<ElementRef>{});
     expect(component).toBeTruthy();
   });
 });
--- src/lib/table/td/td.component.spec.ts
+++ src/lib/table/td/td.component.spec.ts
@@ -1,11 +1,12 @@
+import {DefaultValuePipe} from './../../pipes/default-value/default-value.pipe';
+import { TdComponent } from './td.component';
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async } from '@angular/core/testing';
-import { TdComponent } from './td.component';
 
 describe('Component: Td', () => {
   it('should create an instance', () => {
-    let component = new TdComponent();
+    let component = new TdComponent(new DefaultValuePipe());
     expect(component).toBeTruthy();
   });
 });
