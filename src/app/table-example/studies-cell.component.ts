5ac59c6268980d0f8cd286746a7905a6a97997e7
Gabriel Brandersky <brandersky@interfacewerk>
2016-10-10 17:57:53 +0200

    feat(app.component):  improve and describe examples

---
+++ src/app/table-example/studies-cell.component.ts
@@ -0,0 +1,19 @@
+import {ColumnState} from './../../lib/table/column-state.class';
+
+import {Component, ElementRef, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-studies-cell',
+  templateUrl: 'studies-cell.component.html',
+})
+export class StudiesCellComponent {
+
+  constructor() {}
+
+  @Input() row: any;
+  @Input() col: ColumnState;
+
+  isSubColumnVisible(subcolumn: string): boolean {
+    return (this.col.activeFields.indexOf(subcolumn) !== -1);
+  }
+}
--- src/lib/table/column-state.class.ts
+++ src/lib/table/column-state.class.ts
@@ -2,9 +2,10 @@ import {ColumnConfig} from './table.component';
 
 export class ColumnState {
   // config is read-only, state is stored in other properties
+
   constructor (
     public readonly config: ColumnConfig,
-    public currentSortDirection?: string,
-    public activeFields?: string[]
+    public currentSortDirection: string = 'asc',
+    public activeFields: string[] = []
   ) {}
 }
