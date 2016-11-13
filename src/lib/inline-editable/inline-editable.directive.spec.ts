c78cf80dee4bc6eae94ccc62707c0fc442a0d88a
Gabriel Brandersky <brandersky@interfacewerk.de>
2016-11-13 20:46:12 +0100

    feat(inline-editable): implement basic inline editable directive

---
+++ src/lib/inline-editable/inline-editable.directive.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { InlineEditableDirective } from './inline-editable.directive';
+
+describe('Directive: InlineEditable', () => {
+  it('should create an instance', () => {
+    let directive = new InlineEditableDirective();
+    expect(directive).toBeTruthy();
+  });
+});
