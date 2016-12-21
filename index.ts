e37ed739d99609dd4260db47469c6a97901c5abd
Gabriel Brandersky <brandersky@interfacewerk.de>
2016-12-21 23:20:39 +0100

    fix(activate-event): remove this module

--- index.ts
+++ index.ts
@@ -2,6 +2,5 @@
 export * from './src/lib/table/';
 // directives
 export * from './src/lib/inline-editable/';
-export * from './src/lib/activate-event/';
 // pipes
 export * from './src/lib/pipes/';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import {StudiesCellComponent} from './table-example/studies-cell.component';
 import {AppComponent} from './app.component';
 import {TableModule} from '../lib/table/';
 import {InlineEditableModule} from '../lib/inline-editable/';
-import { ActivateEventDirective } from '../lib/activate-event/';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -16,7 +15,6 @@ import { NgModule } from '@angular/core';
   declarations: [
     AppComponent,
     StudiesCellComponent,
-    ActivateEventDirective,
   ],
   providers: [],
   bootstrap: [AppComponent]
