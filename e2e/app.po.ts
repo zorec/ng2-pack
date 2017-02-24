90012822556281cef31808905d2aa6ebc84d60d8
Gabriel Brandersky <brandersky@interfacewerk.de>
2017-02-25 00:56:59 +0100

    chore: update angular CLI

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,3 +1,11 @@
-export class Ng2PackPage {
+import { browser, element, by } from 'protractor';
 
+export class Ng2PackPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
 }
