1c68f50d65ad60c4130351c4a3aae53fc8458479
Gabriel Brandersky <brandersky@interfacewerk.de>
2017-03-26 14:14:53 +0200

    fix(app): fix ngc errors

--- src/lib/sortable/sortable-item.directive.spec.ts
+++ src/lib/sortable/sortable-item.directive.spec.ts
@@ -1,3 +1,4 @@
+import { ElementRef } from '@angular/core';
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async } from '@angular/core/testing';
@@ -5,7 +6,8 @@ import { SortableItemDirective } from './sortable-item.directive';
 
 describe('Directive: SortableItem', () => {
   it('should create an instance', () => {
-    let directive = new SortableItemDirective();
+    const tmp = document.createElement('div');
+    let directive = new SortableItemDirective(new ElementRef(tmp));
     expect(directive).toBeTruthy();
   });
 });
